cb56965a336bdeb6dfae88ead638cf5016012740
srikanth collaty <srikanthcollaty92@gmail.com>
2021-12-08 15:12:09 +0100

    api de prediction

--- prediction-engine/src/main/java/fr/lernejo/prediction/Temperature_date.java
+++
@@ -1,4 +0,0 @@
-package fr.lernejo.prediction;
-
-public record Temperature_date(int date,double temperature) {
-}
---
+++ prediction-engine/src/main/java/fr/lernejo/prediction/global.java
@@ -0,0 +1,10 @@
+package fr.lernejo.prediction;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public record temperature(ArrayList<Date> Date, ArrayList<Double> Temperature){
+
+
+
+}
