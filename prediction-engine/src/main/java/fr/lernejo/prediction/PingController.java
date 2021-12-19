a81ce0aaba9268c952056628355a80aeb42f4742
srikanth collaty <srikanthcollaty92@gmail.com>
2021-12-19 16:16:17 +0100

    put in uppercase

--- prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
@@ -32,7 +32,7 @@ public class PingController {
     public global getListcountry() {
         gettd();
         getTd1();
-        global gb= new global("france",td);
+        global gb= new global("France",td);
         return gb;
 
 
@@ -42,7 +42,7 @@ public class PingController {
     public void gettd() {
         LocalDate localDate = LocalDate.now();
         TemperatureService ts= new TemperatureService();
-        td.add(new temperatures(localDate, ts.getTemperature("france")));
+        td.add(new temperatures(localDate, ts.getTemperature("France")));
 
     }
 
@@ -51,7 +51,7 @@ public class PingController {
         //LocalDate tomorrow = today.plus(1, ChronoUnit.DAYS);
         LocalDate yesterday = today.minusDays(1);
         TemperatureService ts1= new TemperatureService();
-        td.add(new temperatures(yesterday,ts1.getTemperature("france")));
+        td.add(new temperatures(yesterday,ts1.getTemperature("France")));
 
     }
 }
