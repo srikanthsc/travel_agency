24d7b456bf8b8d3054b2eebbc42e70d0f581d17c
srikanth collaty <srikanthcollaty92@gmail.com>
2021-12-19 13:17:53 +0100

    reduce duplication

--- site/src/main/java/fr/lernejo/travelsite/Catalog_country.java
+++ site/src/main/java/fr/lernejo/travelsite/Catalog_country.java
@@ -26,7 +26,7 @@ public class Catalog_country {
         listcountry1.add(new Travels("Bangladesh", 35.1));
         listcountry1.add(new Travels("Barbados", 35.1));
         listcountry1.add(new Travels("Belarus", 35.1));
-        listcountry1.add(new Travels("Belgium", 35.1));
+       /* listcountry1.add(new Travels("Belgium", 35.1));
         listcountry1.add(new Travels("Belize", 35.1));
         listcountry1.add(new Travels("Benin", 35.1));
         listcountry1.add(new Travels("Bhutan", 35.1));
@@ -59,7 +59,7 @@ public class Catalog_country {
         listcountry1.add(new Travels("Djibouti", 35.1));
         listcountry1.add(new Travels("Dominica", 35.1));
         listcountry1.add(new Travels("Dominican Republic", 35.1));
-        listcountry1.add(new Travels("East Timor", 35.1));
+        listcountry1.add(new Travels("East Timor", 35.1));*/
 
         return listcountry1;
     }
